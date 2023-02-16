0b1bbcebbcae494f7b085eacd8f5883bf1d9f8f8
maryyfry <imarione.tt@gmail.com>
2023-02-16 12:45:37 -0600

    primer test

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve,reject) => {
+ //Identifica si la ruta existe
+ if (fs.existsSync(path)) {
+//Chequear o convertir a una ruta absoluta
+// Probar si esa ruta absoluta es un archivo o un directorio
+// Si es un directorio filtrar los archivos md
+ }else{
+  //Si no existe la ruta se rechaza la promesa
+  reject("La ruta no existe");
+ }
+  });
+};
+
+module.exports =  {
+ mdLinks
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const {mdLinks} = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,5 +6,12 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  // it('deberia devolver una promesa', () => {
+  //   expect(mdLinks()).toBe(typeof Promise);
+  // });
+it('Debe rechazar cuando el path no existe', () => {
+return mdLinks('carmen/cursos/noexiste.md').catch((error) => {
+  expect(error).toBe("La ruta no existe");
+})
+});
 });
