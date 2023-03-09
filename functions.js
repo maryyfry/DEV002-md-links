18c3e67740bb78d4ba0afb225d3d4177b47bf6f6
maryyfry <imarione.tt@gmail.com>
2023-03-09 17:17:20 -0600

    funcion de obtener links

--- functions.js
+++ functions.js
@@ -23,13 +23,35 @@ const mdFile = (absolutePath) => {
     }
 }
 
-// Leer archivo
+// Leer contenido del archivo
 const readFile = (mdFile) => new Promise((resolve, reject) => {
     fs.readFile(mdFile, 'utf-8', (error, file) => {
         if (error) {
             reject(error);
         } else {
-            resolve(file);
+            resolve(file);// devuelve una promesa que se resuelve con el contenido del archivo.
         }
     });
+});
+
+// Obtener links, retorna un array de objetos
+const getLinks = (mdFile) => new Promise((resolve, reject) => {
+    const arrayLinks = [];
+    readFile(mdFile) // Llama a la función readFile
+        .then((file) => {
+            const links = /\[(.+?)\]\((https?:\/\/[^\s)]+)\)/g; // busca coincidencias de enlaces en el contenido del archivo
+            let match = links.exec(file); // devuelve el texto que coincide si se encuentra una coincidencia, de lo contrario, devuelve un valor nulo.
+            while (match !== null) { //bucle
+                arrayLinks.push({
+                    href: match[2], //URL encontrada
+                    text: match[1], //texto encontrado
+                    file: mdFile, //nombre del archivo
+                });
+                match = links.exec(file)//se ejecuta nuevamente la expresión regular 
+            }
+            resolve(arrayLinks);
+        })
+        .catch((error) => {
+            reject(error);
+        })
 });
