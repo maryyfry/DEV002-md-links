9947f328411529454ec2fa2d5b072dff14c9f303
maryyfry <imarione.tt@gmail.com>
2023-03-13 09:29:52 -0600

    funcion md links terminada

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const mdLinks = (path, options) => {
     if (!pathExist(path)) {
       reject('la ruta no existe');  // si no existe la ruta rechaza la promesa
     } else {
+      const absolutePath = toAbsolute(path); // funcion convierte a absoluta
+      if (!mdFile(absolutePath)) {
+        reject('el archivo no es .md') // rechaza si no es un archivo .md
+      } else {
+        // funcion para leer archivos y obtener links en el archivo
+        getLinks(absolutePath).then((arrayLinks) => {
+          if (arrayLinks.length === 0) {
+            reject('no contiene links');
+          } else {
+            if (options === {validate:false}) {
+              resolve(arrayLinks)
+            } else {
+              // funcion para validar links
+              validateLinks(arrayLinks).then((result) => {
+                resolve(result)
+              })   
+            }
+          };
+        })
+
+      };
+    }
+  })
+}
 
 
 module.exports = {
