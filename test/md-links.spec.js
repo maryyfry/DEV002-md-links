bf6cdb737b6d9939b1921139385b6e8e141aa73e
maryyfry <imarione.tt@gmail.com>
2023-03-14 12:07:23 -0600

    primer test arreglado

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,17 +1,18 @@
 const {mdLinks} = require('../index.js');
 
 
+
 describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
-  // it('deberia devolver una promesa', () => {
-  //   expect(mdLinks()).toBe(typeof Promise);
+  // it('Deberia devolver una promesa', () => {
+  //   expect(mdLinks).toBe(typeof Promise);
   // });
-it('Debe rechazar cuando el path no existe', () => {
-return mdLinks('carmen/cursos/noexiste.md').catch((error) => {
-  expect(error).toBe("La ruta no existe");
-})
-});
+  it('Debe debe rechazar cuando el path no existe', () => {
+    return mdLinks('/laura/curso/noexiste.md').catch((error) => {
+      expect(error).toBe("la ruta no existe");
+    })
+  })
 });
